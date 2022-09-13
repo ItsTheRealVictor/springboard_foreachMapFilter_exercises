8dbc52c913ca0290243dd7b10cec3a15698423a0
Victor Delgado <valexdelgado@gmail.com>
2022-09-13 13:57:30 -0700

    set up map branch

---
+++ map.js
@@ -0,0 +1,45 @@
+/*
+map
+doubleValuesWithMap
+Write a function called doubleValuesWithMap which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+doubleValuesWithMap([1,2,3]) // [2,4,6]
+doubleValuesWithMap([1,-2,-3]) // [2,-4,-6]
+function doubleValuesWithMap(arr) {}
+
+valTimesIndex
+Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
+
+valTimesIndex([1,2,3]) // [0,2,6]
+valTimesIndex([1,-2,-3]) // [0,-2,-6]
+extractKey
+Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
+
+extractKey(
+  [
+    {name: 'Elie'},
+    {name: 'Tim'},
+    {name: 'Matt'},
+    {name: 'Colt'}
+  ],
+  'name'
+)
+
+  // ['Elie', 'Tim', Matt', 'Colt']
+extractFullName
+Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of “first” and the value of a key with the name of “last” in each object, concatenated together with a space.
+
+
+extractFullName([
+  {first: 'Elie', last:"Schoppik"},
+  {first: 'Tim', last:"Garcia"},
+  {first: 'Matt', last:"Lane"},
+  {first: 'Colt', last:"Steele"}
+])
+
+  // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
+*/
+
+function doubleValuesWithMap(arr) {
+  return arr;
+}
