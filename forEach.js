7fa1f7b9edd98d451340d23afac1bbe3c2584f56
Victor Delgado <valexdelgado@gmail.com>
2022-09-13 13:52:29 -0700

    initial commit of forEach exercises

---
+++ forEach.js
@@ -0,0 +1,50 @@
+/* doubleValues
+Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+doubleValues([1,2,3]) // [2,4,6]
+doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+onlyEvenValues
+Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
+
+onlyEvenValues([1,2,3]) // [2]
+onlyEvenValues([5,1,2,3,10]) // [2,10]
+showFirstAndLast
+Write a function called showFirstAndLast which accepts an array of strings and returns a new array with only the first and last character of each string.
+
+showFirstAndLast(['colt','matt', 'tim', 'test']) // ["ct", "mt", "tm", "tt"]
+showFirstAndLast(['hi', 'goodbye', 'smile']) // ['hi', 'ge', 'se']
+addKeyAndValue
+Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added for each object
+
+addKeyAndValue(
+[
+  {name: 'Elie'},
+  {name: 'Tim'},
+  {name: 'Matt'},
+  {name: 'Colt'}
+],
+  'title',
+  'instructor'
+)
+
+/*
+  [
+    {name: 'Elie', title:'instructor'},
+    {name: 'Tim', title:'instructor'},
+    {name: 'Matt', title:'instructor'},
+    {name: 'Colt', title:'instructor'}
+  ]
+
+vowelCount
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+
+vowelCount('Elie') // {e:2,i:1};
+vowelCount('Tim') // {i:1};
+vowelCount('Matt') // {a:1})
+vowelCount('hmmm') // {};
+vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1} 
+*/
+
+function doubleValues(arr){
+    return arr
+}
